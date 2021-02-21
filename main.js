baca0ad37777b3d7e32f51044eb092b3015bedbb
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-02-21 22:05:54 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const app = {
+        
+    data() {
+        return {
+            rabbits:[{title:"Netherland Dwaf",img:"images/nd.jpg", done:false},
+                    {title:"Holland Lop",img:"images/hollandlop.jpg", done:false},
+                    {title:"Lionhead",img:"images/lionhead1.jpg", done:false}],
+            copyGallery: [],
+            searchButton: false,
+            search: "",
+            expandImage: false,
+            indexGallery: 0
+        };
+
+    },
+    methods:{
+        appearSerch(){
+            this.appearSerch = !searchButton;
+        },
+        toggleDone(index){
+            this.rabbits[index].done = !this.rabbits[index].done
+        }
+    },
+    computed: {
+        countDone(){
+            return this.rabbits.filter( t => t.done ).length
+        },
+        filterList(){
+            this.copyGallery = this.rabbits.filter((rabbit) => {
+                return rabbit.title.toLowerCase().includes(this.search.toLowerCase());
+            });
+            return this.copyGallery;
+        }
+        
+    }
+    
+
+}
+Vue.createApp(app).mount('#app')
